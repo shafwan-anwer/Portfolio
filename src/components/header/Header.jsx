f7f222ffd52a7ee2ce8c11cbb7a9ae5b472e8e6a
ShafwanAnwer <shafwan.anwer@gmail.com>
2023-01-26 00:22:15 +0530

    main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const header = () => {
     <header>
       <div className='container header_container'>
         <h5>Hello I'm</h5>
-        <h1>Shafwan Anwer</h1>
+        <h1>M Shafwan Anwer</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,63 +12,54 @@ const data = [
     id: 1,
     image: IMG1,
     title: 'Ceilao Admin Panel Web',
-    github: 'https://github.com/shafwan-anwer/AdminPanelweb.git',
     demo: 'http://admin-ceilao.epizy.com'
   },
   {
     id: 2,
     image: IMG2,
     title: 'Ceilao Android App',
-    github: 'https://github.com/ushiradineth/carApp',
     demo: 'https://github.com/ushiradineth/carApp/releases/tag/v1.0'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'E-Commerce Website',
-    github: 'https://github.com/shafwan-anwer/smartshoppers.git',
+    title: ' E-commerce site',
     demo: ''
   },
   {
     id: 4,
     image: IMG4,
     title: 'React Portfolio',
-    github: 'https://github.com/shafwan-anwer/Portfolio',
     demo: 'https://shafwan-anwer.github.io/Portfolio/'
   },
   {
     id: 5,
     image: IMG1,
     title: 'Detox Photography Website',
-    github: '',
     demo: 'https://shafwan-anwer.github.io/photography/'
   },
   {
     id: 6,
     image: IMG2,
     title: 'Shamrock Station Website',
-    github: '',
     demo: 'https://shafwan-anwer.github.io/Shamrock-Station/'
   },
   {
     id: 7,
     image: IMG3,
     title: '2Tuned Website',
-    github: '',
     demo: 'https://shafwan-anwer.github.io/Tuned/'
   },
   {
     id: 8,
     image: IMG1,
     title: 'Appoinment Scheduling Android app',
-    github: '',
     demo: ''
   },
   {
     id: 9,
     image: IMG2,
     title: 'SmartShoppers Inventory App',
-    github: '',
     demo: ''
   }
 
@@ -92,7 +83,6 @@ const portfolio = () => {
             </div>
             <h3>{title}</h3>
             <div className="portfolio_item-cta">
-            <a href={github} className='btn'>Github</a>
             <a href={demo} className='btn btn-primary'>Live Demo</a>
             </div>
           </article>
